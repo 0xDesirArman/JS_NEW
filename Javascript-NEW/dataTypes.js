f3688b63ef9ae177ea32e3fb192ef922d40176cd
SHIVEN <the3xaltedon@gmail.com>
2025-10-29 12:00:43 +0000

    dataTypes

---
+++ Javascript-NEW/dataTypes.js
@@ -0,0 +1,13 @@
+/**
+ string ""
+ bigint
+ number 
+ boolean true false
+ null
+ undefined
+ symbol
+ object
+*/
+
+console.log(typeof null) // gives object
+console.log(typeof undefined) // gives undefined
